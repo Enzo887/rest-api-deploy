0bb91fa7bc8153b8d179e367320327125d55bafa
Enzo887 <enzoduarte2014@gmail.com>
2025-11-18 20:03:33 -0300

    Deply API a Fly

---
+++ app.js
@@ -0,0 +1,104 @@
+const express = require('express')
+const app = express()
+app.use(express.json())
+app.disable('x-powered-by')
+
+const crypto =  require('crypto')
+const cors= require('cors')
+
+app.use(cors({
+    origin: (origin, callback)=>{
+        const ACCEPTED_ORIGINS = [
+            'http://127.0.0.1:5500',
+            'http://localhost:5500',
+            'http://localhost:5501',
+            'http://localhost:5502'
+        ]
+
+        if(ACCEPTED_ORIGINS.includes(origin)){
+            return callback(null, true)
+        }
+        if(!origin){
+            return callback(null, true)
+        }
+        return callback(new Error('Origen no permitido'))
+    }
+}))
+
+
+const movies = require('./movies.json')
+const PORT =  process.env.PORT ?? 1234
+const {validateMovie, validatePartialMovie} = require('./schemas/movies')
+
+
+
+app.get('/movies/:id', (req, res) =>{
+    const {id} = req.params
+    const movieFiltrada = movies.find(movie => movie.id === id)
+    if(movieFiltrada){
+        return res.json(movieFiltrada)
+    }
+    res.status(404).json({ message: 'No se encontro la peli' })
+})
+
+app.get('/movies', (req, res) =>{
+    const {genre} = req.query
+    if(genre){
+        const movieFiltrada = movies.filter(movie => movie.genre.some(g => g.toLowerCase() === genre.toLowerCase()))
+        res.json(movieFiltrada)    
+    }
+    
+    res.json(movies)
+})
+
+app.post('/movies', (req, res) => {
+    const result = validateMovie(req.body)
+
+    if(!result.success){
+        res.status(404).json({ message: 'No se pudo crear la peli'})
+    }
+
+    const nuevaPeli = {
+        id: crypto.randomUUID(),
+        ...result.data
+    }
+
+    movies.push(nuevaPeli)
+    res.json(movies)
+})
+
+app.patch('/movies/:id', (req, res) => {
+    const result = validatePartialMovie(req.body)
+    if(!result.success){
+        return res.status(404).json({ message: 'errorcito uwu'})
+    }
+
+    const {id} = req.params
+    const movieIndex = movies.findIndex(movie => movie.id === id)
+
+    if(movieIndex === -1){
+        return res.status(404).json({ message:'No se encontro la pelicula' })
+    } 
+    const peliCambiada = {
+        ...movies[movieIndex],
+        ...result.data
+    }
+
+    movies[movieIndex] = peliCambiada
+
+    return res.json(peliCambiada)
+})
+
+app.delete('/movies/:id', (req, res) => {
+    const {id} = req.params
+    const movieIndex = movies.findIndex(movie => movie.id === id)
+    if(movieIndex === -1){
+        return res.status(404).json({ message:'No se encontro la pelicula' })
+    }
+    movies.splice(movieIndex, 1)
+    return res.status(204).json({message: 'Pelicula eliminada con exito'})
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Server listening on port  http://localhost:${PORT}`)
+})
---
+++ schemas/movies.js
@@ -0,0 +1,36 @@
+const z =  require('zod')
+
+const movieSchema = z.object({
+    title: z.string({
+        invalid_type_error: "El nombre de la pelicula debe ser un string",
+        require_error: "El nombre de la pelicula es requerido"
+    }),
+    year: z.number().int().min(1900).max(2026),
+    director: z.string(),
+    duration: z.number().int().positive(),
+    rate: z.number().min(0).max(10).default(5),
+    poster: z.url({
+        message: 'El poster debe tener una url valida'
+    }),
+    genre: z.array(
+        z.enum(['Action', 'Adventure','Crime', 'Comedy', 'Drama', 'Fantasy', 'Horro', 'Thriller', 'Sci-Fi']),{
+            required_error: "Es requerido",
+            invalid_type_error: "Debe ser un array de enum"
+        }
+    )
+})
+
+function validateMovie(object){
+    return movieSchema.safeParse(object)
+    // midu recomienda este, devuelve un objeto result si hay error o hay datos
+}
+
+function validatePartialMovie(input){
+    //partial lo que hace es que PONE A TODOS LOS DATOS COMO OPCIONALES, es decir, si el dato esta lo va a validar, y en caso de que no, como es opicional, lo ignora y sigue con lo demas, no pasa nadap.
+    return movieSchema.partial().safeParse(input)
+}
+
+module.exports = {
+    validateMovie,
+    validatePartialMovie
+}
