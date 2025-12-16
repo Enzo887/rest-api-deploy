860d96eeeb7585387623d597e34840546934c81e
Enzo887 <enzoduarte2014@gmail.com>
2025-12-16 19:24:46 -0300

    Agregue informacion de la API

--- app.js
+++ app.js
@@ -9,6 +9,42 @@ app.disable('x-powered-by')
 
 app.use(corsMiddleware())
 
+app.get('/', (req, res) => {
+    res.json({
+        name: 'API REST de Películas',
+        description: 'Proyecto educativo con Node.js y Express',
+        endpoints: {
+        getAll: {
+            method: 'GET',
+            path: '/movies',
+            example: '/movies/?genre=drama'
+        },
+        getById: {
+            method: 'GET',
+            path: '/movies/:id',
+            example: '/movies/c8a7d63f-3b04-44d3-9d95-8782fd7dcfaf',
+            idFormat: 'UUID'
+        },
+        create: {
+            method: 'POST',
+            path: '/movies'
+        },
+        update: {
+            method: 'PATCH',
+            path: '/movies/:id',
+            example: '/movies/c8a7d63f-3b04-44d3-9d95-8782fd7dcfaf',
+            idFormat: 'UUID'
+        },
+        delete: {
+            method: 'DELETE',
+            path: '/movies/:id',
+            example: '/movies/c8a7d63f-3b04-44d3-9d95-8782fd7dcfaf',
+            idFormat: 'UUID'
+        }
+        }
+    })
+})
+
 app.use('/movies', moviesRouter)
 
 app.listen(PORT, ()=>{
